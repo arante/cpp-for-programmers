37a7660721f76cc039417314ed05202b1e0d3567
Billy Wilson Arante <arantebillywilson@gmail.com>
2018-02-06 00:17:46 +0800

    feat: new file define_constants.cpp

---
+++ define_constants.cpp
@@ -0,0 +1,25 @@
+/**
+ * define_constants.cpp
+ *
+ * Use constant variables.
+ *
+ * Written by Billy Wilson Arante <arantebillywilson@gmail.com>
+ * Last updated on 2018/02/06 PHT
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+        // Defining a constant variable
+        const int weightGoal = 100;
+        cout << "Weight goal = " << weightGoal << "\n";
+
+        // Updating a constant variable
+        weigthGoal = 200;
+        cout << "Weight goal = " << weightGoal << "\n";
+
+        return 0;
+}
